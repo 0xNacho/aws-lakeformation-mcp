29a45f1cd5bf9b0fd3e021b513a55ff08b2493fd
0xnacho <nachogarcia91@gmail.com>
2025-05-01 11:08:09 +0200

    Add server tools capabilities

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import { CallToolRequestSchema, ListToolsRequestSchema, Tool } from "@modelcontextprotocol/sdk/types.js";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+
+const GRANT_TOOL: Tool = {
+  name: "grant_lakeformation_permissions",
+  description: "Grants lakeformation permissions to an  user",
+  inputSchema: {
+    type: "object",
+    properties: {
+      table: { type: "string", description: "Name of the table to get permissions on (fotmat: <database_name>.<table_name>" },
+      roleName: { type: "string", default: 3, description: "Name of the role for that will be granted (in arn format)"},
+    },
+    required: ["table", "roleName"],
+  },
+};
+
+// Server setup
+const server = new Server(
+  {
+    name: "aws-lakefomarmation-server",
+    version: "0.1.0",
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  },
+);
+
+server.setRequestHandler(ListToolsRequestSchema, async () => ({
+  tools: [GRANT_TOOL],
+}));
+
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  const { name, arguments: args } = request.params;
+
+  if (name === "gran_lakeformation_permissions") {
+    const { table, roleName} = args as Record<string, string>;
+    try {
+      return {
+        content: [{ type: "text", text: `Granting permissions to ${roleName} on table ${table}` }],
+      }
+    } catch (error) {
+      return {
+        content: [{ type: "text", text: `Error occurred: ${error}` }],
+      };
+    }
+  } else {
+    return {
+      content: [{ type: "text", text: `Unknown tool: ${name}` }],
+      isError: true,
+    };
+  }
+});
+
+// Server startup
+async function runServer() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("AWS KB Retrieval Server running on stdio");
+}
+
+runServer().catch((error) => {
+  console.error("Fatal error running server:", error);
+  process.exit(1);
+});
